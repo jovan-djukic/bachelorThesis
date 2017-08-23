c80e1ae34418a56966b0e9c6929e877933b7a52c
Jovan Djukic <djukicjovan56@gmail.com>
2017-08-23 10:06:25 +0200

    Width parameter is now calculated using clog2 function instead od ternary operator.

--- src/basicCircuits/multiplexer.sv
+++ src/basicCircuits/multiplexer.sv
@@ -1,14 +1,7 @@
 module Multiplexer#(
 	type PORT_TYPE = logic[0 : 0],
 	int NUMBER_OF_INPUTS 		= 4,
-	int SELECT_SIGNAL_WIDTH	= NUMBER_OF_INPUTS <= 4		? 2 :
-														NUMBER_OF_INPUTS <= 8		? 3 :
-														NUMBER_OF_INPUTS <= 16	? 4 : 
-														NUMBER_OF_INPUTS <= 32	? 5 : 
-														NUMBER_OF_INPUTS <= 62	? 6 :
-														NUMBER_OF_INPUTS <= 128 ? 7 :
-														NUMBER_OF_INPUTS <= 256 ? 8 :
-														NUMBER_OF_INPUTS <= 512 ? 9 : 10
+	int SELECT_SIGNAL_WIDTH	= $clog2(NUMBER_OF_INPUTS)
 )(
 	input PORT_TYPE inputSignals[NUMBER_OF_INPUTS],
 	input logic[SELECT_SIGNAL_WIDTH - 1 : 0] select,
