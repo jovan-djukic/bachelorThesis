b29a37faa53c8b5b89c37edd8bf99c978c6c0e2e
Jovan Djukic <djukicjovan56@gmail.com>
2017-08-29 00:32:01 +0200

    Forgot to add this interface. This is a snoopy interface for supplying data.

---
+++ src/memory/readMemoryInterface.sv
@@ -0,0 +1,20 @@
+interface ReadMemoryInterface#(
+	int ADDRESS_WITDH = 32,
+	int DATA_WIDTH    = 32
+)();
+
+	logic[ADDRESS_WITDH - 1 : 0] address;
+	logic[DATA_WIDTH - 1    : 0] dataIn;
+	logic 											 readEnabled, functionComplete;
+
+	modport master (
+		input dataIn, functionComplete,
+		output address, readEnabled
+	);
+
+	modport slave (
+		input address, readEnabled,
+		output dataIn, functionComplete
+	);
+
+endinterface : ReadMemoryInterface
