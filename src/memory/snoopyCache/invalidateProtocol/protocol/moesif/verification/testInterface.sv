e06a6cdaba47f2bf8b37c6cad504c973c27a0438
Jovan Djukic <djukicjovan56@gmail.com>
2017-08-30 00:57:34 +0200

    Founda a bug. In test interface i did not specialize DATA_WIDTH for cache interface. It worked fine for data width up to 16bit, but not for the 32bit. Fixed it. Tested everything again and it is fine.

--- src/memory/snoopyCache/invalidateProtocol/protocol/moesif/verification/testInterface.sv
+++ src/memory/snoopyCache/invalidateProtocol/protocol/moesif/verification/testInterface.sv
@@ -1,11 +1,11 @@
 interface TestInterface#(
 	int ADDRESS_WITDH      = 32,
 	int DATA_WIDTH         = 32,
-	int TAG_WIDTH          = 6,
-	int INDEX_WIDTH        = 6,
-	int OFFSET_WIDTH       = 4,
-	int SET_ASSOCIATIVITY  = 2,
-	int NUMBER_OF_CACHES   = 4,
+	int TAG_WIDTH          = 16,
+	int INDEX_WIDTH        = 8,
+	int OFFSET_WIDTH       = 8,
+	int SET_ASSOCIATIVITY  = 4,
+	int NUMBER_OF_CACHES   = 8,
 	int CACHE_NUMBER_WIDTH = $clog2(NUMBER_OF_CACHES)
 )();
 
@@ -31,6 +31,7 @@ interface TestInterface#(
 		.INDEX_WIDTH(INDEX_WIDTH),
 		.OFFSET_WIDTH(OFFSET_WIDTH),
 		.SET_ASSOCIATIVITY(SET_ASSOCIATIVITY),
+		.DATA_WIDTH(DATA_WIDTH),
 		.STATE_TYPE(CacheLineState),
 		.INVALID_STATE(INVALID)
 	) cacheInterface();
