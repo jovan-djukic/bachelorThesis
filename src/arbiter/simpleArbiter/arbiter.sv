2a6d246e0c4aa300ab549b01e69d56ec9edf5875
Jovan Djukic <djukicjovan56@gmail.com>
2017-08-31 11:37:37 +0200

    Edited simple arbiter.

--- src/arbiter/simpleArbiter/arbiter.sv
+++ src/arbiter/simpleArbiter/arbiter.sv
@@ -17,9 +17,8 @@ module Arbiter#(
 
 	always_comb begin
 		for (int i = 0; i < NUBMER_OF_DEVICES; i++) begin
-			if ((| grants) != 0) begin
-				grants[i] = 0;	
-			end else if (requests[i] == 1) begin
+			grants[i] = 0;
+			 if ((| grants) == 0 && requests[i] == 1) begin
 				grants[i] = 1;
 			end
 		end	
